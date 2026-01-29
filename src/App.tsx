b5ed58d599f1b53177f5720e4a1a1208197846e4
Aryan Khadka <khadkaaryan809@gmail.com>
2026-01-29 16:34:07 +0545

    updated some features

--- src/App.tsx
+++ src/App.tsx
@@ -7,52 +7,69 @@ import {
 } from "react-icons/fa";
 import cover from "./assets/covermain.avif";
 import profile from "./assets/profile.png";
+import icon from "./assets/icon.png";
 function App() {
   const phoneNumber = "9779863167897";
   const message = "Hello! I have a question.";
 
   return (
     <div className="w-full md:max-w-7xl md:mx-auto  min-h-dvh flex justify-center items-center text-[#67536C] font-inter ">
-      <div className="relative flex flex-col w-full max-w-[480px] md:mx-auto border-[#67536C]/50 rounded-4xl border-2 mx-4 p-1.5">
+      <div className="relative flex flex-col w-full max-w-[480px] md:mx-auto border-[#67536C]/50 rounded-4xl border-2 mx-4 p-1.5 overflow-hidden">
         <div className="relative w-full">
-          <div className="w-full h-[135px] sm:h-[160px]">
+          <div className="group w-full h-33.75 sm:h-40">
             <img
               src={cover}
               alt=""
               className="h-full w-full object-cover rounded-4xl"
             />
           </div>
-          <div className="absolute w-[120px] sm:w-[115px] border-0 border-black left-4 -bottom-[40%]">
-            <img
-              src={profile}
-              alt=""
-              className="h-full w-full border-white border-6 object-cover rounded-full hover:scale-[1.05] transition-all"
-            />
+          <div className="absolute left-4 -bottom-[40%]">
+            <div className="relative group w-29 rounded-full overflow-hidden border-6 border-white bg-[#353535]">
+              <img
+                src={icon}
+                alt=""
+                className="h-full  w-full object-cover rounded-full group-hover:scale-[1.05] opacity-0 group-hover:opacity-100 transition-all duration-300"
+              />
+              <img
+                src={profile}
+                alt=""
+                className="absolute top-0  h-full w-full object-cover rounded-full group-hover:scale-[1.05]  group-hover:opacity-0 transition-all duration-300"
+              />
+            </div>
           </div>
         </div>
 
         <div className="flex flex-col text-left mt-16 sm:mt-18 gap-4 sm:gap-5 px-6">
           {/* intro */}
           <div className="flex justify-between gap-2 items-center font-poppins">
-            <div className="space-y-0">
+            <div className="space-y-1">
               <h1 className="font-bold text-[22px] sm:text-[24px] text-[#1F242F] leading-6.5">
                 Ronash Chitrakar
               </h1>
-              <h3 className="opacity-90 text-[6B7387] text-[14px] sm:text-[16px] cursor-pointer">
-                <a href="https://www.instagram.com/tentacles.v?igsh=ZTNuZWYydWc3eXM1">
-                
+              <div className="relative group opacity-90  text-[14px] sm:text-[16px] cursor-pointer overflow-hidden ">
+                <a
+                  href="https://www.instagram.com/tentacles.v?igsh=ZTNuZWYydWc3eXM1"
+                  className="text-[6B7387] z-50"
+                >
                   @tentacles.v
                 </a>
-              </h3>
+                {/* <div className="absolute top-0 h-6 w-[210px] -translate-x-full group-hover:translate-x-0 transition-all duration-300 border bg-[#353535] z-10"></div> */}
+              </div>
             </div>
-            <div>
+            <div className="group">
               <a
                 href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <button className="px-5 py-2 bg-[#67536C]  text-white rounded-full cursor-pointer hover:bg-[#353535] transition-all hover:scale-[1.05]">
-                  Contact
+                <button className=" relative w-[110px] h-[40px] bg-[#67536C]  text-white rounded-full cursor-pointer group-hover:bg-[#353535]  transition-all overflow-hidden">
+                  <span className="absolute top-1/2 left-1/2 -translate-1/2 group-hover:-top-full transition-all duration-300">
+                    {" "}
+                    Contact
+                  </span>
+                  <span className="absolute top-full left-1/2 -translate-x-1/2 translate-y-full group-hover:top-1/2 group-hover:-translate-y-1/2  transition-all duration-300">
+                    Whatsapp
+                  </span>
                 </button>
               </a>
             </div>
@@ -61,8 +78,7 @@ function App() {
           <div className="">
             <h2>
               <span className="pr-2 font-bold text-[18px] sm:text-[20px]">
-                {" "}
-                PROFILE{" "}
+                PROFILE
               </span>
               <span className="border-l-2 border-[#67536C]  pl-2 text-[16px] sm:text-[18px] opacity-80">
                 STRATEGY SPECIALIST
@@ -129,7 +145,8 @@ function App() {
             href="https://www.tiktok.com/@trentttfr?_r=1&_t=ZS-93NzLruZL0u"
             target="_blank"
             rel="noopener noreferrer"
-            className="transition transform hover:scale-115 hover:text-black ">
+            className="transition transform hover:scale-115 hover:text-black "
+          >
             <FaTiktok />
           </a>
         </div>
